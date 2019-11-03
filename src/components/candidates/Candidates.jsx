070a29b6d8ad0e772d06a163690bdecbbb906b92
angelamhkim <31169340+angelamhkim@users.noreply.github.com>
2019-11-03 13:04:05 -0800

    Update Candidates.jsx

--- src/components/candidates/Candidates.jsx
+++ src/components/candidates/Candidates.jsx
@@ -40,37 +40,37 @@ export default class Candidates extends Component {
             <h2 className={style.name}> Joe Biden </h2>
             <img src={Joe} alt="Joe" className={style.images} />
             <p> Joe cares about... </p>
-            <p> Join Joe's campaign!</p>
+            <p> Join <a href="https://joebiden.com/">Joe's campaign!</a></p>
           </div>
           <div className="card col-sm-12 col-md-4">
             <h2 className={style.name}> Elizabeth Warren </h2>
             <img src={Elizabeth} alt="Elizabeth" className={style.images} />
             <p> Elizabeth cares about... </p>
-            <p> Join Elizabeth's campaign!</p>
+            <p> Join <a href="https://elizabethwarren.com/">Elizabeth's campaign!</a></p>
           </div>
           <div className="card col-sm-12 col-md-4">
             <h2 className={style.name}> Kamala Harris </h2>
             <img src={Kamala} alt="Kamala" className={style.images} />
             <p> Kamala cares about... </p>
-            <p> Join Kamala's campaign!</p>
+            <p> Join <a href="https://kamalaharris.org/">Kamala's campaign!</a></p>
           </div>
           <div className="card col-sm-12 col-md-4">
             <h2 className={style.name}> Andrew Yang </h2>
             <img src={Andrew} alt="Andrew" className={style.images} />
             <p> Andrew cares about... </p>
-            <p> Join Andrew's campaign!</p>
-          </div>
-          <div className="card col-sm-12 col-md-4">
-            <h2 className={style.name}> Donald Trump </h2>
-            <img src={Trump} alt="Donald" className={style.images} />
-            <p> Donald cares about... </p>
-            <p> Join Donald's campaign!</p>
+            <p> Join <a href="https://www.yang2020.com/">Andrew's campaign!</a></p>
           </div>
           <div className="card col-sm-12 col-md-4">
             <h2 className={style.name}> Pete Buttigieg </h2>
             <img src={Pete} alt="Pete" className={style.images} />
             <p> Pete cares about... </p>
-            <p> Join Pete's campaign!</p>
+            <p> Join <a href="https://peteforamerica.com/">Pete's campaign!</a></p>
+          </div>
+          <div className="card col-sm-12 col-md-4">
+            <h2 className={style.name}> Donald Trump </h2>
+            <img src={Trump} alt="Donald" className={style.images} />
+            <p> Donald cares about... </p>
+            <p> Join <a href="https://www.donaldjtrump.com/">Donald's campaign!</a></p>
           </div>
         </div>
       </div>
